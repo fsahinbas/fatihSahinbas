57f050e2581938107c3bcde888b8487daa2981b2
fsahinbas <fsahinbas@gmail.com>
2021-07-12 17:20:34 +0300

    6. adımdaki değişiklik yapıldı

--- fatih.py
+++ fatih.py
@@ -2,4 +2,5 @@ import json
 file=open("fatih.json","r")
 json_file=json.load(file)
 obj=json_file["kimlik"]
-print(obj["Ad"],obj["Soyad"],sep="\n")
+#print(obj["Ad"],obj["Soyad"],sep="\n")
+print(obj)
